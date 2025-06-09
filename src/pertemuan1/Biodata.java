83f8bb3bc0a66395eb088c2306138d7168fdc385
mhmdzayi <zainimuhmmd023gmail.com>
2025-06-09 19:47:44 +0800

    merapikan code

--- src/pertemuan1/Biodata.java
+++ src/pertemuan1/Biodata.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pertemuan1;
 
-/**
- *
- * @author lenovo
- */
 public class Biodata {
+    public static void main(String[] args){
+        System.out.println("=======Data Diri=======");
+        System.out.println("Nama   : Muhammad Zaini");
+        System.out.println("NPM    : 2310010302");
+        System.out.println("Kelas  : 4C");
+        System.out.println("Alamat : Berangas Timur");
+        
+    }
     
 }
