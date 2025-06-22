13a589d80e2dcd11f1e1b7a1aacb31870411eed8
mhmdzayi <zainimuhmmd023gmail.com>
2025-06-22 18:55:10 +0800

    Membuat Class Main untuk menjalankan Program

--- src/pertemuan2/Main.java
+++ src/pertemuan2/Main.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pertemuan2;
 
-/**
- *
- * @author lenovo
- */
 public class Main {
-    
+    public static void main(String[] args) {
+        Mahasiswa z = new Mahasiswa();
+        z.nama = "Muhammad Zaini";
+        z.npm = "2310010302";
+        z.jurusan = "Teknik Informatika";
+        
+        //Menampilkan informasi Mahasiswa
+        System.out.println("Nama    :" +z.nama);
+        System.out.println("NPM     :" +z.npm);
+        System.out.println("Jurusan :" +z.jurusan);
+       
+    }
 }
